c81e2b02b5564ded3cd529e6afff4820948c8b51
Sugam Ghising <ghisingsugam04@gmail.com>
2026-02-22 22:31:24 +0545

    feat(routes): add hotels route to central route registry

--- src/routes/registerRoutes.ts
+++ src/routes/registerRoutes.ts
@@ -2,6 +2,7 @@ import { healthRoutes, userRoutes } from '@api/index';
 import { config } from '@config/index';
 import { Router } from 'express';
 import { authRoutes } from '../api/auth';
+import { hotelsRoutes } from '../api/hotel';
 import { organizationRoutes } from '../api/organizations';
 
 const router = Router();
@@ -19,6 +20,7 @@ const v1Router = Router();
 v1Router.use('/users', userRoutes);
 v1Router.use('/organizations', organizationRoutes);
 v1Router.use('/auth', authRoutes);
+v1Router.use('/hotels', hotelsRoutes);
 
 // Mount versioned routes
 router.use(config.api.fullPrefix, v1Router);
